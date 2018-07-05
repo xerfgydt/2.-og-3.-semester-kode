67855bd86799d215730081f950145f0ca04d1e22
xerfgydt <rune1005@gmail.com>
2018-07-05 11:44:08 +0200

    Add files via upload

---
+++ Graphs/src/IGraph.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public interface IGraph<V> {
+    public void addVertex(V vertex);
+    public V getVertex(int v);
+    public void addEdge(int u , int v);
+    public List<V> getVertices();
+    public Tree dfs(int v);
+    public void clear();
+
+
+
+}
---
+++ Graphs/src/Tree.java
@@ -0,0 +1,27 @@
+public class Tree {
+    private int root;
+    private boolean[] visited;
+
+    public Tree(int root, boolean[] visited) {
+        this.root = root;
+        this.visited = visited;
+    }
+
+    public boolean areAllVerticesVisited(){
+        int count = 0;
+        for (int i = 0; i < visited.length ; i++) {
+            if(visited[i]){
+                count++;
+            }
+        }
+        return count == visited.length;
+    }
+    public String toString(){
+        String out = "";
+        for (int i = 0; i < visited.length ; i++) {
+            out += "Vertex: " + i + " visited: " + visited[i] + "´\n" ;
+        }
+        return out;
+    }
+
+}
